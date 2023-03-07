a5cba9ce2466a4fe6f64dad29249d67c1a7cee0f
Deivy Potosi <deibypotosi535@gmail.com>
2023-03-06 22:51:41 -0500

    Proyecto Terminado Version 1.0

---
+++ js/Dato.js
@@ -0,0 +1,22 @@
+class Dato {
+    constructor(descripcion,valor){
+        this._description = descripcion;
+        this._valor = valor;
+    };
+
+    get descripcion(){
+        return this._description;
+    }
+
+    set descripcion(descripcion){
+        this._description = descripcion;
+    }
+
+    get valor(){
+        return this._valor;
+    };
+
+    set valor(valor){
+        this._valor = valor;
+    }
+}
